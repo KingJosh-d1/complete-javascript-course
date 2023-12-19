eb4c3322fb27fb2ba7f1e2ea94bee86db9385f9c
KingJosh-d1 <adebayojo19@gmail.com>
2023-12-19 06:35:06 +0100

    Commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -10,16 +11,51 @@ console.log(firstName)
 console.log(firstName)
 console.log(firstName)
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
 let person = "jonas"
 let PI = 3.1415;
 
-let my_FirstJob = "Programmer";
+let my_FirstJob = " Coder";
 let myCurrentJob = "Teacher";
 
 let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(my_FirstJob)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
